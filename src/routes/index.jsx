5bdaa31672d80d8d8a6aeecb3f8617719d0bbe47
virugamacoder <krunal.virugama.inventyv@gmail.com>
2025-09-27 12:14:29 +0530

    Refactor routing structure

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,8 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import ROUTE_PATH from "./ROUTE_PATH";
 import RootLayout from "layouts/rootLayout";
-import { SecurityTokenDetails, SecurityTokensPage } from "pages/index";
-import IndividualKYCPage from "pages/kyc/individualKyc";
-import CorporateKYCPage from "pages/kyc/corporateKyc";
-import LiquidityPage from "pages/liquidity/liquidityList";
-import LiquiditySettings from "pages/liquidity/liquiditySettings";
-import AddLiquidity from "pages/liquidity/addLiquidity";
+import ComponentsPage from "pages/Components";
+import { AddLiquidity, CorporateKYCPage, IndividualKYCPage, InvestmentDetailPage, InvestmentsPage, KYCPage, LiquidityPage, LiquiditySettings, MarketDetailPage, PopularMarketsPage, SecurityTokenDetails, SecurityTokensPage, SwapPage } from "pages";
 
 const Router = createBrowserRouter([
     {
@@ -16,14 +12,21 @@ const Router = createBrowserRouter([
         // errorElement: <ErrorPage />,
         children: [
             { index: true, element: <Navigate to={ROUTE_PATH.SECTURITY_TOKENS} /> },
+            { path: `${ROUTE_PATH.KYC}`, element: <KYCPage /> },
             { path: `${ROUTE_PATH.KYC_INDIVIDUAL}`, element: <IndividualKYCPage /> },
             { path: `${ROUTE_PATH.KYC_CORPORATE}`, element: <CorporateKYCPage /> },
+            { path: `${ROUTE_PATH.INVESTMENTS}`, element: <InvestmentsPage /> },
+            { path: `${ROUTE_PATH.INVESTMENTS}/:id`, element: <InvestmentDetailPage /> },
             { path: `${ROUTE_PATH.SECTURITY_TOKENS}`, element: <SecurityTokensPage /> },
             { path: `${ROUTE_PATH.SECTURITY_TOKENS}/:id`, element: <SecurityTokenDetails /> },
+            { path: `${ROUTE_PATH.POPULAR_MARKETS}`, element: <PopularMarketsPage /> },
+            { path: `${ROUTE_PATH.POPULAR_MARKETS}/:id`, element: <MarketDetailPage /> },
             { path: `${ROUTE_PATH.LIQUIDITY}`, element: <LiquidityPage /> },
             { path: `${ROUTE_PATH.LIQUIDITY_SETTINGS}`, element: <LiquiditySettings /> },
             { path: `${ROUTE_PATH.ADD_LIQUIDITY}`, element: <AddLiquidity /> },
             { path: `${ROUTE_PATH.SWAP}`, element: <SwapPage /> },
+            { path: `${ROUTE_PATH.SWAP_SETTINGS}`, element: <LiquiditySettings /> },
+            { path: `${ROUTE_PATH.COMPONENTS}`, element: <ComponentsPage /> },
         ],
     },
 ])
