703f64a0c9ad9117efcab2f5c2dea9c37528f489
virugamacoder <krunal.virugama.inventyv@gmail.com>
2025-09-27 10:52:31 +0530

    Assets add for logo social media and all

--- src/assets/images/index.js
+++
@@ -1,5 +0,0 @@
-import HeaderLogo from './Logo.png';
-import FooterLogo from './Logo.png';
-
-export const img_header_logo = HeaderLogo;
-export const img_footer_logo = FooterLogo;
---
+++ src/assets/images/index.jsx
@@ -0,0 +1,171 @@
+import img_header_logo from "./img_header_logo.png";
+import img_arrowright336 from "./img_arrowright336.svg";
+import img_polygonlogofreelogovectorsnet_1 from "./img_polygonlogofreelogovectorsnet_1.png";
+import img_metamask_fox_1 from "./img_metamask_fox_1.svg";
+import img_facebook from "./img_facebook.svg";
+import img_trash from "./img_trash.svg";
+import img_warning_black_400 from "./img_warning_black_400.svg";
+import img_info_black_400 from "./img_info_black_400.svg";
+import img_footer_logo from "./img_footer_logo.png";
+import img_search from "./img_search.svg";
+import img_arrowdown from "./img_arrowdown.svg";
+import img_filter from "./img_filter.svg";
+import img_image_60x60 from "./img_image_60x60.png";
+import img_secure_svgrepo_com from "./img_secure_svgrepo_com.png";
+import img_image_230x408 from "./img_image_230x408.png";
+import img_image_1 from "./img_image_1.png";
+import img_image from "./img_image.png";
+import img_arrow_down_brand_color_1 from "./img_arrow_down_brand_color_1.svg";
+import img_image_2 from "./img_image_2.png";
+import img_globe from "./img_globe.svg";
+import img_music from "./img_music.svg";
+import img_group_30 from "./img_group_30.svg";
+import img_warning from "./img_warning.svg";
+import img_info from "./img_info.svg";
+import img_image_438x780 from "./img_image_438x780.png";
+import img_image_48x48 from "./img_image_48x48.png";
+import img_arrow_up from "./img_arrow_up.svg";
+import img_checkmark from "./img_checkmark.svg";
+import img_info_circle_svgrepo_com from "./img_info_circle_svgrepo_com.svg";
+import img_vector from "./img_vector.png";
+import img_vector_8x14 from "./img_vector_8x14.png";
+import img_copy_svgrepo_com from "./img_copy_svgrepo_com.png";
+import img_metamaskfox_1_gray_900_02 from "./img_metamaskfox_1_gray_900_02.svg";
+import img_mail_svgrepo_com from "./img_mail_svgrepo_com.png";
+import img_document_1_svgrepo_com from "./img_document_1_svgrepo_com.png";
+import img_eye_svgrepo_com from "./img_eye_svgrepo_com.svg";
+import img_vector_12x6 from "./img_vector_12x6.png";
+import img_image_72x72 from "./img_image_72x72.png";
+import img_copysvgrepocom_1 from "./img_copysvgrepocom_1.png";
+import img_ellipse_24_32x32 from "./img_ellipse_24_32x32.png";
+import img_arrow_right_336_white_a700 from "./img_arrow_right_336_white_a700.svg";
+import img_user from "./img_user.svg";
+import img_image_12 from "./img_image_12.png";
+import img_image_20x20 from "./img_image_20x20.png";
+import img_close from "./img_close.svg";
+import img_close_md_svgrepo_com from "./img_close_md_svgrepo_com.svg";
+import img_vector_20x20 from "./img_vector_20x20.png";
+import img_light_bulb from "./img_light_bulb.svg";
+import img_calendar_svgrepo_com from "./img_calendar_svgrepo_com.png";
+import img_image_1_40x36 from "./img_image_1_40x36.png";
+import img_image_5_40x40 from "./img_image_5_40x40.png";
+import img_image_34x34 from "./img_image_34x34.png";
+import img_vector_40x40 from "./img_vector_40x40.png";
+import img_image_10 from "./img_image_10.png";
+import img_image_14 from "./img_image_14.png";
+import img_delete from "./delete.svg";
+import img_image_3 from "./img_image_3.png";
+import correct_icon from "./correct_icon.svg";
+import wallet_img from "./wallet_img.svg";
+import building_img from "./building.svg";
+import user_img from "./user.svg";
+import user_round from "./user_round.svg";
+import building_dark from "./building-dark.svg";
+import building_gradient from "./building-gradient.svg";
+import mail_dark from "./mail-dark.svg";
+import mail_gradient from "./mail-gradient.svg";
+import telegram_dark from "./telegram-dark.svg";
+import telegram_gradient from "./telegram-gradient.svg";
+import correct_icon_edges from "./correct_icon_edges.svg";
+import building_round from "./building_round.svg";
+import circle_filled from "./circle_filled.svg";
+import circle_empty from "./circle_empty.svg";
+import checkbox_filled from "./checkbox_filled.svg";
+import market_1 from "./market_1.png";
+import market_2 from "./market_2.png";
+import market_3 from "./market_3.png";
+import market_4 from "./market_4.png";
+import market_detail from "./market_detail.png";
+import up_direction from "./up_direction.svg";
+import down_direction from "./down_direction.svg";
+import flat_direction from "./flat_direction.svg";
+import correct_circle from "./correct_circle.svg";
+import empty_circle from "./empty_circle.svg";
+import error_image from "./error_image.svg";
+
+export {
+    img_header_logo,
+    img_arrowright336,
+    img_polygonlogofreelogovectorsnet_1,
+    img_metamask_fox_1,
+    img_facebook,
+    img_trash,
+    img_warning_black_400,
+    img_info_black_400,
+    img_footer_logo,
+    img_search,
+    img_arrowdown,
+    img_filter,
+    img_image_60x60,
+    img_secure_svgrepo_com,
+    img_image_230x408,
+    img_image_1,
+    img_image,
+    img_arrow_down_brand_color_1,
+    img_image_2,
+    img_globe,
+    img_music,
+    img_group_30,
+    img_warning,
+    img_info,
+    img_image_438x780,
+    img_image_48x48,
+    img_arrow_up,
+    img_checkmark,
+    img_info_circle_svgrepo_com,
+    img_vector,
+    img_vector_8x14,
+    img_copy_svgrepo_com,
+    img_metamaskfox_1_gray_900_02,
+    img_mail_svgrepo_com,
+    img_document_1_svgrepo_com,
+    img_eye_svgrepo_com,
+    img_vector_12x6,
+    img_image_72x72,
+    img_copysvgrepocom_1,
+    img_ellipse_24_32x32,
+    img_arrow_right_336_white_a700,
+    img_user,
+    img_image_12,
+    img_image_20x20,
+    img_close,
+    img_close_md_svgrepo_com,
+    img_vector_20x20,
+    img_light_bulb,
+    img_calendar_svgrepo_com,
+    img_image_1_40x36,
+    img_image_5_40x40,
+    img_image_34x34,
+    img_vector_40x40,
+    img_image_10,
+    img_image_14,
+    img_delete,
+    img_image_3,
+    correct_icon,
+    wallet_img,
+    building_img,
+    user_img,
+    user_round,
+    building_dark,
+    building_gradient,
+    mail_dark,
+    mail_gradient,
+    telegram_dark,
+    telegram_gradient,
+    correct_icon_edges,
+    building_round,
+    circle_filled,
+    circle_empty,
+    checkbox_filled,
+    market_1,
+    market_2,
+    market_3,
+    market_4,
+    market_detail,
+    up_direction,
+    down_direction,
+    flat_direction,
+    correct_circle,
+    empty_circle,
+    error_image
+}
